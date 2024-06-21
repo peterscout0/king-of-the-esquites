df38aaeddc11402c4de718a7effe252c18bea7dc
Pedro <peterscout1.1@gmail.com>
2024-06-21 12:07:48 -0600

    Created footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faInstagram, faTwitter, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'king-of-the-esquites';
+
+  addresses = [
+    { line1: "Carretera Interamericana zona 4", line2: "Jutiapa, Jutiapa" },
+    { line1: "Central del progreso", line2: "El progreso, Jutiapa" },
+    { line1: "Centro Comercial de Jalapa", line2: "Jalapa, Jalapa" }
+  ];
+  
+  contacts = [
+    { info: "Whatsapp", phone: "1234-4567" },
+    { info: "Servicio a Domicilio", phone: "1234-4567" }
+  ];
+  
+  operatingHours = [
+    { days: "Lunes a Viernes", time: "8AM-9PM" },
+    { days: "Sábados y Domingos", time: "10AM-6PM" }
+  ];
+  
+  socialLinks = [
+    { iconPrefix: 'fab', iconName: 'facebook-f' },
+    { iconPrefix: 'fab', iconName: 'instagram' },
+    { iconPrefix: 'fab', iconName: 'twitter' },
+    { iconPrefix: 'fab', iconName: 'tiktok' }
+  ];
+  
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebookF, faInstagram, faTwitter, faTiktok);
+  }
+
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgFor } from '@angular/common'; 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, NgFor],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  @Input() addresses!: any[];
+  @Input() contacts!: any[];
+  @Input() operatingHours!: any[];
+  @Input() socialLinks!: any[];
 }
