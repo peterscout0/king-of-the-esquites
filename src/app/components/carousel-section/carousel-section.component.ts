4494664e5a3c0fc643524129358590ecabfd184b
Pedro <peterscout1.1@gmail.com>
2024-07-02 17:10:56 -0600

    Filter changes to the image of people, not to everything, carousel section

--- src/app/components/carousel-section/carousel-section.component.ts
+++ src/app/components/carousel-section/carousel-section.component.ts
@@ -39,7 +39,6 @@ export class CarouselSectionComponent implements OnInit {
     return {
       '--background-color': this.backgroundColor,
       '--background-image': this.showBackgroundImage ? `url(${this.backgroundImage})` : 'none',
-      'filter': this.imageFilter,
       ...(this.parallax && { 'background-attachment': 'fixed' }),
     };
   }
